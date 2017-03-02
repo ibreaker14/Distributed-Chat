52cadea77a6bb30cf3036357beb2b9d12958e04b
ibreaker14 <minijordon@gmail.com>
2017-03-01 23:21:33 -0800

    added comments and mutex

--- src/Chat.java
+++ src/Chat.java
@@ -6,53 +6,68 @@ import org.json.*;
 
 import java.util.concurrent.locks.*;
 /*****************************//**
-* \brief It implements a distributed chat. 
+* \brief This program implements a distributed chat. 
 * It creates a ring and delivers messages
 * using flooding 
+* \author	Mingtau Li, 011110539
+* \author	Mark Spencer Tan, 012192282
+* \author	Kevin Duong, 011715000
 **********************************/
 public class Chat{
 
-	// My info
+	/// process alias
 	public static String myAlias = null;
+	/// process port
 	public static int myPort = 0;
-	// Successor
+	// process successor ip
 	public static String ipSuccessor = null;
+	/// process successor port
 	public static int portSuccessor = 0;
-	// Predecessor
+	/// process predecessor ip
 	public static String ipPredecessor = null;
+	/// process predecessor port
 	public static int portPredecessor = 0;
-
+	/// IP information
 	public static String localhost = "127.0.0.1";
+	/// Sentinel for joining chatroom
 	private static boolean joined = false;
-		
-	/*new class - Server===================================================================================================================*/
-
-/*****************************//**
-* \class Server class "chat.java" 
-* \brief It implements the server
-**********************************/ 
-	private static class Server implements Runnable{
-		private Socket clntSock = null;
-		private ServerSocket servSock = null;
-		private String alias = null;
-		private int port =0;
-		String operation = null;
-		JSONObject msg = null;
-
-		public Server(String a, int p) {
-			alias = a;
-			port = p;
-		}
+	/// mutex lock
+	private static final Lock mutex = new ReentrantLock(true);
 		
 	/*****************************//**
-	* \brief It allows the system to interact with the participants. 
-	**********************************/   
+	* \class Server class "chat.java" 
+	* \brief It implements the server
+	**********************************/ 
+	private static class Server implements Runnable{
+		/// client socket
+		private Socket clntSock = null;
+		/// server socket
+		private ServerSocket servSock = null;
+		/// operation type
+		String operation = null;
+		/// message received
+		JSONObject msg = null;
+
+		/*****************************//**
+		* \brief It initializes constructor for Server
+		* \param a alias of server for unique identification
+		* \param p server port 
+		**********************************/   
+		public Server(String a, int p) {
+			myAlias = a;
+			myPort = p;
+		}
+		
+		/*****************************//**
+		* \brief It allows the system to interact with the participants. 
+		**********************************/   		
 		public void run() {     
 			try{
 			//create the server socket
-			servSock = new ServerSocket(port);
+			servSock = new ServerSocket(myPort);
 			while (true) {
-				clntSock = servSock.accept(); // Get client connections
+				// Get client connections
+				clntSock = servSock.accept(); 
 				try{
 					//Create io streams
 					ObjectInputStream  ois = new ObjectInputStream(clntSock.getInputStream());
@@ -68,21 +83,28 @@ public class Chat{
 
 					//does operations based on json commands
 					switch(operation){ 
-						case "JOIN":
+						//join chat
+						case "JOIN":	
 							try{
 
 								JSONObject acceptObj = JSONMessage("ACCEPT",localhost,portPredecessor);//write accept msg to my pred
 								JSONObject newSuccessorObj = JSONMessage("NEWSUCCESSOR",localhost,((JSONObject)msg.get("parameters")).getInt("myPort")); //new predecessor
 
-								portPredecessor = ((JSONObject)msg.get("parameters")).getInt("myPort"); //replace my pred with To_Join
+								mutex.lock();
+								//update predecessor
+								portPredecessor = ((JSONObject)msg.get("parameters")).getInt("myPort"); 
+								mutex.unlock();
 
-								oos.writeObject(acceptObj.toString()); //send to my pred (to client: my pred)
+								//send to my pred (to client: my pred)
+								oos.writeObject(acceptObj.toString()); 
 
+								//close sockets
 								clntSock.close();
+
+								// open new socket and send message to predecessor for acceptance request
 								clntSock = new Socket(localhost,((JSONObject)acceptObj.get("parameters")).getInt("portPred")); //send to my old predecessor
 								oos = new ObjectOutputStream(clntSock.getOutputStream());
 								oos.writeObject(newSuccessorObj.toString());
-								clntSock.close();
 
 							}catch(IOException e){
 								e.printStackTrace();
@@ -92,48 +114,64 @@ public class Chat{
 
 							break;
 
-						case "NEWSUCCESSOR":
+						//new successor
+						case "NEWSUCCESSOR":	
 							try{
+								mutex.lock();
+								// connect to new successor
 								portSuccessor = ((JSONObject)msg.get("parameters")).getInt("portSuccessor");
+								mutex.unlock();
 
 							}catch(JSONException e){
 								e.printStackTrace();
 							}
 							break;
-						case "PUT":
+						//send message
+						case "PUT":		
 							try {
 								
+								//sender information
 								String message = ((JSONObject)msg.get("parameters")).getString("message");
 								String receiverAlias = ((JSONObject)msg.get("parameters")).getString("aliasReceiver");
 								String senderAlias = ((JSONObject)msg.get("parameters")).getString("aliasSender");
 
+								//if sender is self, it client is not in chat
 								if(senderAlias.equalsIgnoreCase(myAlias)) {
 									System.out.println("\nClient has either been disconnected or cannot be found");
 									System.out.println("\nWhat would you like to do? \n 1) LEAVE\n 2) SEND MESSAGE\n 3) DISPLAY PROFILE");
-								}else if(receiverAlias.equalsIgnoreCase(alias)) {
+								//if I am the receiver, display message
+								}else if(receiverAlias.equalsIgnoreCase(myAlias)) {
 									System.out.println("\n--> "+senderAlias+": "+message);
 									System.out.println("\nWhat would you like to do? \n 1) LEAVE\n 2) SEND MESSAGE\n 3) DISPLAY PROFILE");
 									
 								}else {
+									//send messages to successor
 									clntSock = new Socket(localhost,portSuccessor);
 									oos = new ObjectOutputStream(clntSock.getOutputStream());
 									oos.writeObject(msg.toString());
+
 								}
 							} catch (JSONException e) {
 								e.printStackTrace();
 							}
 								
 							break;
-						case "LEAVE":
+						//leave chatroom
+						case "LEAVE":	
 							try{
+								mutex.lock();
+								//connect to new predecessor
 								portPredecessor = ((JSONObject)msg.get("parameters")).getInt("portPred");
-
+								mutex.unlock();
+								//close socket
 								clntSock.close();
+
+								//send new successor info to new predecessor
 								clntSock = new Socket(localhost,portPredecessor); //send to my updated predecessor
 								JSONObject newSuccessorObj = JSONMessage("NEWSUCCESSOR",localhost,myPort); //old predecessor's new successor is me
 								oos = new ObjectOutputStream(clntSock.getOutputStream());
 								oos.writeObject(newSuccessorObj.toString());
-								clntSock.close();
+
 								Scanner scan = new Scanner(System.in);
 
 
@@ -145,6 +183,7 @@ public class Chat{
 
 
 							break;
+						// default behavior for currupted messages
 						default:
 							System.out.print("System has encountered a problem --> ");
 							System.out.println("message"+msg);
@@ -156,7 +195,9 @@ public class Chat{
 					oos.close();
 					ois.close();
 
+					//close socket
 					clntSock.close();
+
 				}catch(NullPointerException e){
 					e.printStackTrace();
 					System.out.println("Server exception");
@@ -170,32 +211,34 @@ public class Chat{
 			}catch(ClassNotFoundException e){
 			e.printStackTrace();
 			}
-		}//end server run
-	}//end Server class
-	
+		}
+	}
 
-/*new class - client ================================================================================================================*/
-	
-/*****************************//*
-* \brief It implements the client
-**********************************/
-	//private class Client implements Runnable {       
+	/*****************************//**
+	* \class Client class "chat.java" 
+	* \brief It implements the client
+	**********************************/ 
 	private static class Client implements Runnable {       
-	
+	/// accepts user input
 	private Scanner scan;
-	
+	/// stream writing
 	private ObjectOutputStream oos = null;
+	/// stream for reading
 	private ObjectInputStream ois = null; 
-
+	/// ip inet address
 	private InetAddress ip = null;
+	/// socket for connection
 	private Socket socket = null;
 
-	private String alias = null;
-	private int port = 0;
 
+	/*****************************//**
+	* \brief It initializes constructor for Client
+	* \param a alias of server for unique identification
+	* \param p server port 
+	**********************************/  
 	public Client(String a, int p){
-		alias = a;
-		port  = p;
+		myAlias = a;
+		myPort  = p;
 		scan  = new Scanner(System.in);
 	}
 
@@ -226,13 +269,19 @@ public class Chat{
 						ois = new ObjectInputStream(socket.getInputStream()); 
 
 						//Create JSON Message. Attempt to join
-						JSONObject joinObj = JSONMessage("JOIN",alias, myPort); 
+						JSONObject joinObj = JSONMessage("JOIN",myAlias, myPort); 
 						oos.writeObject(joinObj.toString()); //send To_Join's port 
+
+						mutex.lock();
+						//update successor
 						portSuccessor = port;	
+						mutex.unlock();
 
 						
 						JSONObject acceptedMsg = new JSONObject(ois.readObject().toString()); //from server: accept (ip, portPred)
+						mutex.lock();
 						portPredecessor = ((JSONObject)acceptedMsg.get("parameters")).getInt("portPred");
+						mutex.unlock();
 						socket.close();
 
 						joined = true; //if exception occurs before completion of join, joined stays false
@@ -253,7 +302,8 @@ public class Chat{
 						// Connect to the server socket
 						ip = InetAddress.getByName(localhost);
 
-						socket = new Socket(ip,portSuccessor); //connect to my successor
+						//connect to my successor
+						socket = new Socket(ip,portSuccessor); 
 
 						// Create streams
 						oos = new ObjectOutputStream(socket.getOutputStream()); 
@@ -261,16 +311,20 @@ public class Chat{
 
 						//Create JSON Message. Attempt to leave
 						JSONObject leaveObj = JSONMessage("LEAVE",localhost, portPredecessor); 
-						oos.writeObject(leaveObj.toString()); //send To_Join's port 
+						oos.writeObject(leaveObj.toString()); 
 
-						portSuccessor = myPort; //redirect successor to myself
-						portPredecessor = myPort; //redirect predecessor to myself	
-						socket.close(); 
+						//redirect predecessor and successor to myself
+						mutex.lock();
+						portSuccessor = myPort;
+						portPredecessor = myPort; 
+						mutex.unlock();
+
+						socket.close();
+						oos.close();
+
 						System.out.println(myAlias+" ("+myPort+") has left the conversation");
 						
 						joined = false; //upon successful leave, joined becomes false
-
-						System.out.println("\n(PRED, SUCC): "+portPredecessor+", " +portSuccessor);
 
 					}catch(ConnectException e){
 						System.out.println("Can't connect to this port");
@@ -287,10 +341,13 @@ public class Chat{
 				if(!joined){
 					System.out.println("\nYou must join a port first");
 				}else{
+					//stops client from sending invalid messages
 					boolean validMessage = true;
+					//saves receiver alias
 					String receiverAlias = null;
 					scan.nextLine();
 					
+					//asks user for message. message is not valid if you send to yourself
 					do{
 						System.out.println("Who do you want to send a message to?");
 						receiverAlias = scan.nextLine();
@@ -302,21 +359,27 @@ public class Chat{
 						}
 					}while(!validMessage);
 
+					//asks user for message
 					System.out.println("What do you want to say?");
 					String message = scan.nextLine();
 					System.out.println("Message sent");
-					//Create JSON Message
-					JSONObject sendObj = JSONMessage("PUT",alias, myPort,receiverAlias.trim(),message.trim()); 
 
+					//Create JSON Message for senging message
+					JSONObject sendObj = JSONMessage("PUT",myAlias, myPort,receiverAlias.trim(),message.trim()); 
+
+					//send message to successor
 					socket = new Socket(ip,portSuccessor);
 					oos = new ObjectOutputStream(socket.getOutputStream());
 					oos.writeObject(sendObj.toString());
+					socket.close();
+					oos.close();
 					
 				}
 				break;
 
 			case 3: //DISPLAY PROFILE
 				
+				//display alias, port, predecessor, and successsor
 				System.out.println("\n" + myAlias.toUpperCase() +" at port: " +myPort);
 				if(joined){
 					System.out.println("Predecessor: "+portPredecessor+"  Successor: "+portSuccessor);
@@ -329,27 +392,26 @@ public class Chat{
 			ois.close();
 			oos.close();
 
+			//close client socket
+			socket.close();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}catch(Exception e){
 			//e.printStackTrace();
 		}
 		}
-		/* TODO Use mutex to handle race condition when reading and writing the global variable (ipSuccessor, 
-		portSuccessor, ipPredecessor, portPredecessor)*/
-	}
-	}
-	
-	
-/*****************************//**
-* Starts the threads with the client and server:
-* \param Id unique identifier of the process
-* \param port where the server will listen
-**********************************/  
-	public Chat(String myAlias, int myPort) {
 
-		this.myAlias = myAlias;
-		this.myPort = myPort;
+	}
+	}
+	
+	
+	/*****************************//**
+	* Starts the threads with the client and server:
+	* \param myAlias unique identifier of the process
+	* \param myPort where the server will listen
+	**********************************/  
+	public Chat(String myAlias, int myPort) {
 
 		this.ipSuccessor = localhost;
 		this.ipPredecessor = localhost;
@@ -358,8 +420,9 @@ public class Chat{
 
 
 		// Initialization of the peer
-		Thread server = new Thread(new Server(this.myAlias, this.myPort));
-		Thread client = new Thread(new Client(this.myAlias, this.myPort));
+		Thread server = new Thread(new Server(myAlias, myPort));
+		Thread client = new Thread(new Client(myAlias, myPort));
+		// start threads
 		server.start();
 		client.start();
 		try {
@@ -372,16 +435,23 @@ public class Chat{
 
 	public static void main(String[] args) throws JSONException {
 
-	      if (args.length < 2 ) {  
+		//throws exception when no arguments are found
+	    if (args.length < 2 ) {  
 		throw new IllegalArgumentException("Parameter: <myAlias> <myPort>");
 	}
 
+	// start chat
 	Chat chat = new Chat(args[0], Integer.parseInt(args[1]));
-
 
 	}
 	
-	// Creates a JSON Message 
+	/*****************************//**
+	* \brief Creates JSON Messages based on type
+	* \param type message type
+	* \param alias my alias
+	* \param port my port
+	* \param msgArgs vararg for additional information
+	**********************************/ 
 	public static JSONObject JSONMessage(String type, String alias, int port, String... msgArgs){
 		JSONObject  obj = new JSONObject();
 		JSONObject params = new JSONObject();
@@ -414,40 +484,32 @@ public class Chat{
 		}
 
 
-		//keeps asking user for valid integer
-		public static int validInt(Scanner in, String message, int... limit){			
-			int n = 0;
-			while(true){
-				System.out.println(message + ": ");
-				
-				try{
-					n = in.nextInt();
-						if (limit.length > 0 && n > limit[0]){
-						throw new NumberFormatException("number too large");
-					}else{
-						break;
-					}
-				}catch(InputMismatchException e){
-					System.out.println("not a valid input. Try again");
-					in.next(); //consumes return char
-				}catch(NumberFormatException e){
-					System.out.println("not a valid input. Try again");
+	/*****************************//**
+	* \brief Asks user for valid numerical input
+	* \param in scanner object
+	* \param message message displayed to user
+	* \param limit optional limit on number length
+	**********************************/ 
+	public static int validInt(Scanner in, String message, int... limit){			
+		int n = 0;
+		while(true){
+			System.out.println(message + ": ");
+			
+			try{
+				n = in.nextInt();
+					if (limit.length > 0 && n > limit[0]){
+					throw new NumberFormatException("number too large");
+				}else{
+					break;
 				}
+			}catch(InputMismatchException e){
+				System.out.println("not a valid input. Try again");
+				in.next(); //consumes return char
+			}catch(NumberFormatException e){
+				System.out.println("not a valid input. Try again");
 			}
-			return n;
 		}
+		return n;
+	}
 }
 
-
-
-
-
-// mutex example
-
-/*private final Lock _mutex = new ReentrantLock(true);
-
-_mutex.lock();
-
-// your protected code here
-
-_mutex.unlock();*/
